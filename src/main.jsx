d0d4baa05e7d8fdea9d451c89865169f25ba42de
grumel <grumel@gmail.com>
2026-07-20 22:30:31 +0200

    Fix CSS import order for redesigned UI

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import './shell-v2.css'
+import './app-v2.css'
 import { getTheme, applyTheme } from './theme.js'
 
 // Theme früh anwenden (vor dem ersten Render), damit kein Aufblitzen entsteht.
