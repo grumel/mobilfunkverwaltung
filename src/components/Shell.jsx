5372dbb67bafc2eae041ab911cb1af105287b624
grumel <grumel@gmail.com>
2026-07-20 21:46:33 +0200

    Modernize remaining application surfaces

--- src/components/Shell.jsx
+++ src/components/Shell.jsx
@@ -13,6 +13,7 @@ import HelpModal from './HelpModal.jsx'
 import UnmatchedDevices from './UnmatchedDevices.jsx'
 import { getTheme, toggleTheme } from '../theme.js'
 import '../shell-v2.css'
+import '../app-v2.css'
 
 const FILTER_LABELS = {
   alle: 'Alle Teilnehmer', verified: 'Geprüft', ohne_gsm: 'Ohne GSM', ohne_name: 'Ohne Name',
